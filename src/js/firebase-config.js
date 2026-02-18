160884374984375d05a18abaa4807bf6ac2705e0
SenithDamiru <senithdamiru2005@gmail.com>
2026-02-19 00:33:52 +0530

    Furniture Shopping Page
    Created the fully functional furniture shopping

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyAxbudofAkh4BDu0SPnXW7mEAOMRzb-F_o",
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
